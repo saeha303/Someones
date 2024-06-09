f7dc10104ca4f45eb61b0a8b3259c0a4f7af4f07
saeha303 <sumaiyasaeha@gmail.com>
2024-06-09 22:58:48 +0600

    6

--- api/index.js
+++ api/index.js
@@ -3,14 +3,7 @@ import cors from "cors";
 
 const app = Express();
 
-app.use(cors(
-    {
-        origin: 'https://someones-oaxq.vercel.app',
-        methods:["GET","POST","PUT","DELETE","OPTIONS"],
-        allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-        credentials: true
-      }
-));
+app.use(cors());
 
 
 app.get("/api/hello/", (req, res) => {
