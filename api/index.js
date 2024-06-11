b308ee4e82200912d0f00569baeb88b3a4de1311
saeha303 <sumaiyasaeha@gmail.com>
2024-06-11 23:07:16 +0600

    17

--- api/index.js
+++ api/index.js
@@ -3,13 +3,13 @@ import cors from "cors";
 
 const app = Express();
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+// const corsOptions ={
+//     origin:'*', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
  
- app.use(cors(corsOptions))
+//  app.use(cors(corsOptions))
 
 
 app.get("/api/hello/", (req, res) => {
